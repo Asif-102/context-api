bbf695f5c2de25bb3f4abef1ce4f51dce16d4742
Asif2535 <aiqbal517@gmail.com>
2021-03-08 01:57:54 +0600

    filter data based on dynamic context change

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,13 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { CategoryContext } from '../App';
 import CategoryDetail from '../CategoryDetail/CategoryDetail';
 
+const allProducts = [
+    {name:'Lenovo', category:'laptop'},{name:'Asus', category:'laptop'},{name:'Dell', category:'laptop'},
+    {name:'Samsung', category:'mobile'},{name:'Nokia', category:'mobile'},{name:'Apple', category:'mobile'},
+    {name:'Canon', category:'camera'},{name:'Nikkon', category:'camera'},{name:'Sony', category:'camera'}
+]
+
 const Categories = () => {
-    const category = useContext(CategoryContext);
+    const [category] = useContext(CategoryContext);
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        const matchedProducts = allProducts.filter(pd=> pd.category.toLowerCase() === category.toLowerCase());
+        setProducts(matchedProducts);
+    },[category])
     return (
         <div>
             <h2>Select your category: {category}</h2>
-            <CategoryDetail></CategoryDetail>
+            {
+                products.map(pd => <CategoryDetail product={pd}></CategoryDetail>)
+            }
         </div>
     );
 };
